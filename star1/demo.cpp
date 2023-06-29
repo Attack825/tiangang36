812793e9b970aac6147cdc8bc037194dbb1b95ee
文杰 <2707138687@qq.com>
2023-06-29 14:08:33 +0800

    test

---
+++ star1/demo.cpp
@@ -0,0 +1,27 @@
+#include <graphics.h>		// 引用图形库头文件
+#include <conio.h>
+#include <iostream> //控制台输入输出
+
+
+//C语言天罡三十六题，基础九题
+//天魁星，绘制七巧版图案
+
+void draw() {
+	
+}
+
+
+
+int main()
+{
+	initgraph(640, 480);	// 创建绘图窗口，大小为 640x480 像素
+	circle(200, 200, 100);	// 画圆，圆心(200, 200)，半径 100
+	
+	
+	
+	_getch();				// 按任意键继续
+	closegraph();			// 关闭绘图窗口
+	return 0;
+
+
+}
